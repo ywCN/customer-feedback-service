fded0ee74adf6bc8a83b410fcb02098d10c29249
ywCN <wangyouhao90@gmail.com>
2017-12-29 17:33:22 -0500

    dummy app

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // on server side, we use commonJS modules
 // on front end side we can use ES2015 modules like import...
 const express = require('express');
+const app = express();
 
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
 
+app.listen(5000);
