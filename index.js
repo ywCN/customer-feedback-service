727a988b6241f87ef715ecc83074f9f722887174
ywCN <wangyouhao90@gmail.com>
2017-12-29 18:22:14 -0500

    express and node

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 const express = require('express');
 const app = express();
 
+// The arrow function will be called when
+// the Route with '/' is being visited.
 app.get('/', (req, res) => {
     res.send({ hi: 'there' });
 });
 
-app.listen(5000);
+app.listen(5000); // Express tells Node to list to the port 5000
