54f601908f8d60831b0d85332ddbadbf2f08a8c9
ywCN <wangyouhao90@gmail.com>
2017-12-29 21:05:00 -0500

    import modules and set up passport strategy

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // on server side, we use commonJS modules
 // on front end side we can use ES2015 modules like import...
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
+
+passport.use(new GoogleStrategy());
 
 // This is a route handler. The arrow function will 
 // be called when the Route with '/' is being visited.
