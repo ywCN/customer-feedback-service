85131a6b5bbf001b4329e517a1d4e8d6eda33805
ywCN <wangyouhao90@gmail.com>
2017-12-31 16:17:01 -0500

    full url is also bad

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        {/* Relative link will not work since the server uses 5000 port. */}
+        {/* Relative link will not work since the server uses 5000 port.
+        http://localhost:5000/auth/google is also bad because it only work for dev. */}
         <a href="http://localhost:5000/auth/google">Sing In With Google</a>
       </div>
     );
