336de009ecaf846a3c9a7992656b1080b9c03280
ywCN <wangyouhao90@gmail.com>
2018-01-01 17:31:45 -0500

    3 return results

--- client/src/reducers/authReducers.js
+++ client/src/reducers/authReducers.js
@@ -4,6 +4,7 @@ export default function(state = null, action) {
     // console.log(action);
     switch (action.type) {
         case FETCH_USER:
+            // returns either null, false, or user modal
             return action.payload || false; // ''||false => false
         default:
             return state;
