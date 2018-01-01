e0a84fc8f4622a2fd67db98285e69a8e09b966fe
ywCN <wangyouhao90@gmail.com>
2018-01-01 17:34:09 -0500

    reason of props.auth

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducers';
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer // auth will be in the props if connect by a Component
 });
