3cd4fe377c0ca20003a930256c5b2a8a3bd68d08
ywCN <wangyouhao90@gmail.com>
2018-01-01 16:43:32 -0500

    refactor 1

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-    return function(dispatch) {
+export const fetchUser = () =>
+    function(dispatch) {
         // routes/authRouts.js
         axios
             .get('/api/current_user')
             .then(res => dispatch({ type: FETCH_USER, payload: res }));
     };
-};
