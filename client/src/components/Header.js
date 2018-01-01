4079cf7b2982013929b76d324168913202b94cc4
ywCN <wangyouhao90@gmail.com>
2018-01-01 17:34:47 -0500

    refactor

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,8 +19,8 @@ class Header extends Component {
     }
 }
 
-function mapStateToProps(state) {
-    return { auth: state.auth };
+function mapStateToProps({ auth }) {
+    return { auth };
 }
 
 export default connect()(Header);
