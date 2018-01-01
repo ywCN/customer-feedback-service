9164f5990208c3432ff3e50c10f3429c819f5c4e
ywCN <wangyouhao90@gmail.com>
2018-01-01 13:36:34 -0500

    add route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,18 @@ const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-    return <div>Hi!</div>;
+    // the outside div is for CSS
+    return (
+        <div>
+            {/* BrowserRouter can have at most 1 child. */}
+            <BrowserRouter>
+                <div>
+                    {/* Root Route */}
+                    <Route path="/" component={Landing} />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
