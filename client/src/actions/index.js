f32390188de0550dfa8750aa59a4ca783f49f61a
ywCN <wangyouhao90@gmail.com>
2018-01-02 17:25:22 -0500

    action creator detail

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,7 +5,7 @@ export const fetchUser = () => async dispatch => {
     // path is from routes/authRouts.js
     // res is request
     const res = await axios.get('/api/current_user');
-    // we only need the data property of th request
+    // we only need the data property of the request
     dispatch({ type: FETCH_USER, payload: res.data });
 };
 
@@ -18,8 +18,11 @@ export const fetchUser = () => async dispatch => {
 //     };
 // };
 
+// this action creator takes a token and sends it to our backend api
 export const handleToken = token => async dispatch => {
+    // this respond is the updated user model with updated credit
     const res = await axios.post('/api/stripe', token);
 
+    // dispatch this action with FETCH_USER type to reducers
     dispatch({ type: FETCH_USER, payload: res.data });
 };
