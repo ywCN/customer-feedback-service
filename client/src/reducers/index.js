4342615063215a5b83e05884cfde7fcc05487958
ywCN <wangyouhao90@gmail.com>
2018-01-02 16:48:09 -0500

    Redux state property

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducers';
 
 export default combineReducers({
-    auth: authReducer // auth will be in the props if connect by a Component
+    auth: authReducer // auth will be one of the property of Redux state object
 });
