2369ecadc8d7068d00d2f2ac9df2f4ccd2d666da
ywCN <wangyouhao90@gmail.com>
2018-01-02 17:14:08 -0500

    move comment location

--- client/src/reducers/authReducers.js
+++ client/src/reducers/authReducers.js
@@ -1,9 +1,9 @@
-// This reducer produces the 'auth' property of Redux state object.
 import { FETCH_USER } from '../actions/types';
 
 export default function(state = null, action) {
     // console.log(action);
     switch (action.type) {
+        // This reducer produces the 'auth' property of Redux state object.
         case FETCH_USER:
             // returns either null, false, or user modal
             return action.payload || false; // ''||false => false
