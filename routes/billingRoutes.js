01ba7577f3ed21dfb07ddebceccc13c364eecceb
ywCN <wangyouhao90@gmail.com>
2018-01-02 15:13:22 -0500

    add reference

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,6 +4,13 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 module.exports = app => {
     // request handler
     app.post('/api/stripe', (req, res) => {
-        console.log(req.body);
+        // https://stripe.com/docs/api#create_charge
+        // https://www.npmjs.com/package/stripe#using-promises
+        stripe.charges.create({
+            amount: 500,
+            currency: 'usd',
+            decription: '$5 for 5 credits',
+            source: req.body.id
+        });
     });
 };
