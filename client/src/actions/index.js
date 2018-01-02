d7cca51ee36d70d6d23e8537f3a255893ea3d01e
ywCN <wangyouhao90@gmail.com>
2018-01-01 23:08:01 -0500

    handleToken

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -17,3 +17,9 @@ export const fetchUser = () => async dispatch => {
 //             .then(res => dispatch({ type: FETCH_USER, payload: res }));
 //     };
 // };
+
+export const handleToken = token => async dispatch => {
+    const res = await axios.post('/api/stripe', token);
+
+    dispatch({ type: FETCH_USER, payload: res.data });
+};
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -13,6 +13,7 @@ class Payments extends Component {
                 token={token => console.log(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
             >
+                {/* override default button */}
                 <button className="btn">Add Credits</button>
             </StripeCheckout>
         );
