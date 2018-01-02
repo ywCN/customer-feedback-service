118560a8fd9d134a43019c74a473ed415d8c7f3f
ywCN <wangyouhao90@gmail.com>
2018-01-02 15:46:09 -0500

    make sure user is logged in

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,6 +4,10 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 module.exports = app => {
     // (req, res) =>{} is the request handler
     app.post('/api/stripe', async (req, res) => {
+        // make sure user is logged in
+        if (!req.user) {
+            return res.status(401).send({ error: 'You must log in!' });
+        }
         // https://stripe.com/docs/api#create_charge
         // https://www.npmjs.com/package/stripe#using-promises
         const charge = await stripe.charges.create({
