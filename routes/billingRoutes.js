57992e93d154d97fc29ca8fbc38446fe266d0d59
ywCN <wangyouhao90@gmail.com>
2018-01-02 15:01:29 -0500

    test parsebody middleware

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,5 +2,8 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
-    app.post('/api/stripe', (req, res) => {});
+    // request handler
+    app.post('/api/stripe', (req, res) => {
+        console.log(req.body);
+    });
 };
