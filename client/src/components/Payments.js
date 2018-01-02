9214ce2d6c3956194d6d714bf8a11c5130f87c16
ywCN <wangyouhao90@gmail.com>
2018-01-01 22:56:19 -0500

    override default button

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -12,7 +12,9 @@ class Payments extends Component {
                 amount={500}
                 token={token => console.log(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
+            >
+                <button className="btn">Add Credits</button>
+            </StripeCheckout>
         );
     }
 }
