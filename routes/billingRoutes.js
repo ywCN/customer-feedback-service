74f29e025382b41af8a493c027ebaa29bf56e20f
ywCN <wangyouhao90@gmail.com>
2018-01-02 15:14:58 -0500

    async charge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,15 +2,17 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
-    // request handler
-    app.post('/api/stripe', (req, res) => {
+    // (req, res) =>{} is the request handler
+    app.post('/api/stripe', async (req, res) => {
         // https://stripe.com/docs/api#create_charge
         // https://www.npmjs.com/package/stripe#using-promises
-        stripe.charges.create({
+        const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
             decription: '$5 for 5 credits',
             source: req.body.id
         });
+
+        console.log(charge);
     });
 };
