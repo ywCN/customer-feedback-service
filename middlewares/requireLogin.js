bc0c534689deb9520278b6389f55157f94021907
ywCN <wangyouhao90@gmail.com>
2018-01-02 15:55:11 -0500

    middleware checks login status

---
+++ middlewares/requireLogin.js
@@ -0,0 +1,5 @@
+// req: request
+// res: response
+// next: a function will be called when middleware finishes its work
+// next can be used for done or chaining on next middleware
+module.exports = (req, res, next) => {};
