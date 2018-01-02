ffbc47e2caba50fed7b3313dd7e24414dc627059
ywCN <wangyouhao90@gmail.com>
2018-01-02 16:01:29 -0500

    build module that checks login status

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -2,4 +2,11 @@
 // res: response
 // next: a function will be called when middleware finishes its work
 // next can be used for done or chaining on next middleware
-module.exports = (req, res, next) => {};
+module.exports = (req, res, next) => {
+    if (!req.user) {
+        // 401: forbidden
+        return res.status(401).send({ error: 'You must log in!' });
+    }
+
+    next();
+};
