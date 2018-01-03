594feab6ad6d40addc8d7f80987016b893912c46
ywCN <wangyouhao90@gmail.com>
2018-01-03 12:25:58 -0500

    use the new middleware to check credits after checking log in status

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,8 @@
 const requireLogin = require('../middlewares/requireLogin'); // a middleware
+const requireCredits = require('../middlewares/requireCredits'); // a middleware
 
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, (req, res) => {});
+    // The order of middlewares matter because they will be executed one by one
+    // In this case, we need to make sure user is logged in first then check credits.
+    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {});
 };
