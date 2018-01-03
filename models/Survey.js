a00b22e9c1179550d27ac65b534a24e732aa843d
ywCN <wangyouhao90@gmail.com>
2018-01-03 10:41:01 -0500

    set up relationship

--- models/Survey.js
+++ models/Survey.js
@@ -8,7 +8,8 @@ const surveySchema = new Schema({
     subject: String,
     recipients: [RecipientSchema],
     yes: { type: Number, default: 0 },
-    no: { type: Number, default: 0 }
+    no: { type: Number, default: 0 },
+    _user: { type: Schema.Types.ObjectId, ref: 'User' } // _ means this is a relationship
 });
 
 // load this up into the mongoose library
