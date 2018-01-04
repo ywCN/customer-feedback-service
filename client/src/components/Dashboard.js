e3da3a6ebd8db37723ddfd8d12bb4d0f479d2c46
ywCN <wangyouhao90@gmail.com>
2018-01-04 17:16:22 -0500

    icon

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,6 +6,7 @@ const Dashboard = () => {
             Dashboard
             <div className="fixed-action-btn">
                 <a className="btn-floating btn-large red">
+                    {/* icon names: http://materializecss.com/icons.html */}
                     <i className="material-icons">add</i>
                 </a>
             </div>
