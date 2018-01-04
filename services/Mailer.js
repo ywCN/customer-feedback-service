dc108bda8dcf32a0ec6f2530ae3a6b61fb56d9d9
ywCN <wangyouhao90@gmail.com>
2018-01-03 19:53:26 -0500

    set up Mailer except formatAddresses helper function

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,6 +2,16 @@ const sendgrid = require('sendgrid');
 const helper = sendgrid.mail; // helps create Mailer class
 const keys = require('../config/keys');
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+    // first argument is a destructured object
+    // second argument is a html String
+    constructor({ subject, recipients }, content) {
+        super();
+        this.from_email = new helper.Email('no-reply@emaily.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+    }
+}
 
 module.exports = Mailer;
