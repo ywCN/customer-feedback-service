1f9b6bbb68aa24c9b17b36f1a2dd8f7e64b5c52b
ywCN <wangyouhao90@gmail.com>
2018-01-04 14:45:18 -0500

    use route

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,20 +1,20 @@
 const keys = require('../../config/keys');
 module.exports = survey => {
     return `
-        <html>
-            <body>
-                <div style="text-align: center;">
-                    <h3>I'd like your feedback!</h3>
-                    <p>Please answer the following question:</p>
-                    <p>${survey.body}</p>
-                    <div>
-                        <a href="${keys.redirectDomain}">Yes</a>
-                    </div>
-                    <div>
-                        <a href="${keys.redirectDomain}">No</a>
-                    </div>
+    <html>
+        <body>
+            <div style="text-align: center;">
+                <h3>I'd like your feedback!</h3>
+                <p>Please answer the following question:</p>
+                <p>${survey.body}</p>
+                <div>
+                    <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
                 </div>
-            </body>
-        </html>
+                <div>
+                    <a href="${keys.redirectDomain}/api/surveys/thanks">No</a>
+                </div>
+            </div>
+        </body>
+    </html>
     `;
 };
