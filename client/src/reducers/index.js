01396561ae14a6c395d5066e477d38645584657d
ywCN <wangyouhao90@gmail.com>
2018-01-04 18:06:52 -0500

    combine reducer provided by redux-form

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as reduxForm } from 'redux-form'; // provided by redux-form lib
 import authReducer from './authReducers';
 
+// these keys are property names of the one Redux state object
 export default combineReducers({
-    auth: authReducer // auth will be one of the property of Redux state object
+    auth: authReducer, // auth will be one of the property of Redux state object
+    form: reduxForm
 });
