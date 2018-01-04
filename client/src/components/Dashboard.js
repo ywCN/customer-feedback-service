d959a4338c96b69b05b6b63a8c989ccb1762c830
ywCN <wangyouhao90@gmail.com>
2018-01-04 17:07:57 -0500

    add button

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,7 +6,7 @@ const Dashboard = () => {
             Dashboard
             <div className="fixed-action-btn">
                 <a className="btn-floating btn-large red">
-                    <i className="large material-icons">add</i>
+                    <i className="material-icons">add</i>
                 </a>
             </div>
         </div>
