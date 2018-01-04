ef7fec83f19adf90107b72b4e09d4294ef85c633
ywCN <wangyouhao90@gmail.com>
2018-01-04 18:53:55 -0500

    comment Field props

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,14 +1,24 @@
 // SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form'; // like connect
+import { reduxForm, Field } from 'redux-form';
 
 class SurveyForm extends Component {
     render() {
-        return <div>SurveyForm!</div>;
+        return (
+            <div>
+                {/* Field require props to know how to render itself. The name
+                property can be any String. The name property tells redux that
+                we have one piece of data being produced by our form called
+                "surveyTitle". The component property tells redux-form that this
+                Field should appear as an HTML input tag. The type property
+                means the input need to be text. */}
+                <Field type="text" name="surveyTitle" component="input" />
+            </div>
+        );
     }
 }
 
-// reduxForm takes only one argument
+// reduxForm is like connect(), takes only one argument
 export default reduxForm({
     form: 'surveyForm'
 })(SurveyForm);
