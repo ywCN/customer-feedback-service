c7ab9b5c3f25b3e16e1c4ae638bb57837d9e403b
ywCN <wangyouhao90@gmail.com>
2018-01-04 17:19:11 -0500

    add route to /survey/new

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
         <div>
             Dashboard
             <div className="fixed-action-btn">
-                <a className="btn-floating btn-large red">
+                <Link to="/surveys/new" className="btn-floating btn-large red">
                     {/* icon names: http://materializecss.com/icons.html */}
                     <i className="material-icons">add</i>
-                </a>
+                </Link>
             </div>
         </div>
     );
