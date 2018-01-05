8812922b95f1fbea783ccbf42ae9f0e21027afe2
ywCN <wangyouhao90@gmail.com>
2018-01-04 19:25:49 -0500

    add event handlers

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,11 @@
 // SurveyField contains logic to render a single label and text input
 import React from 'react';
 
-export default () => {
+// destructure, input is one property that contains a lot of event handlers
+export default ({ input }) => {
     return (
         <div>
-            <input />
+            <input {...input} />
         </div>
     );
 };
