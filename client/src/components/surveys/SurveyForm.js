c69bf9ced776af86be1c293f649e7ea2445580f3
ywCN <wangyouhao90@gmail.com>
2018-01-05 17:55:02 -0500

    use fields.js

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,18 +4,12 @@ import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 import validateEmails from '../../utils/validateEmails';
-
-const FIELDS = [
-    { label: 'Survey Title', name: 'title' },
-    { label: 'Subject Line', name: 'subject' },
-    { label: 'Email Body', name: 'body' },
-    { label: 'Recipient List', name: 'emails' }
-];
+import formFields from './formFields';
 
 class SurveyForm extends Component {
     renderFields() {
         // ({ label, name }) destructures each object in the FIELDS array
-        return _.map(FIELDS, ({ label, name }) => {
+        return _.map(formFields, ({ label, name }) => {
             return (
                 <Field
                     component={SurveyField}
@@ -67,7 +61,7 @@ function validate(values) {
     errors.emails = validateEmails(values.emails || ''); // || '' avoid undefined
 
     // for each field
-    _.each(FIELDS, ({ name }) => {
+    _.each(formFields, ({ name }) => {
         if (!values[name]) {
             // [name] means property, not 'name' property
             // key interpolation???
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
 
 const SurveyFormReview = ({ onCancel, formValues }) => {
     return (
         <div>
             <h5>Please confirm.</h5>
-            <div>
-                <label>Survey Title</label>
-                <div>{formValues.title}</div>
-            </div>
-            <div>
-                <label>Subject Line</label>
-                <div>{formValues.subject}</div>
-            </div>
-            <div>
-                <label>Email Body</label>
-                <div>{formValues.body}</div>
-            </div>
-            <div>
-                <label>Recipents</label>
-                <div>{formValues.emails}</div>
-            </div>
+
             <button className="yellow darken-3 btn-flat" onClick={onCancel}>
                 Back
             </button>
