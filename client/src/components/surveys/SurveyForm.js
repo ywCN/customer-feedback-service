d75b6c71903ff6a36eff51f860d752e210719b8c
ywCN <wangyouhao90@gmail.com>
2018-01-04 19:04:41 -0500

    add button

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -6,13 +6,21 @@ class SurveyForm extends Component {
     render() {
         return (
             <div>
-                {/* Field require props to know how to render itself. The name
+                {/* handleSubmit is injected by reduxForm() at bottom */}
+                <form
+                    onSubmit={this.props.handleSubmit(values =>
+                        console.log(values)
+                    )}
+                >
+                    {/* Field require props to know how to render itself. The name
                 property can be any String. The name property tells redux that
                 we have one piece of data being produced by our form called
                 "surveyTitle". The component property tells redux-form that this
                 Field should appear as an HTML input tag. The type property
                 means the input need to be text. */}
-                <Field type="text" name="surveyTitle" component="input" />
+                    <Field type="text" name="surveyTitle" component="input" />
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         );
     }
