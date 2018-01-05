6d39edf46b64d210978bd3f5e4e4b1a899a44eae
ywCN <wangyouhao90@gmail.com>
2018-01-04 19:14:10 -0500

    use SurveyField

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,8 +1,17 @@
 // SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 
 class SurveyForm extends Component {
+    renderFields() {
+        return (
+            <div>
+                <Field type="text" name="title" component={SurveyField} />
+            </div>
+        );
+    }
+
     render() {
         return (
             <div>
@@ -17,8 +26,9 @@ class SurveyForm extends Component {
                 we have one piece of data being produced by our form called
                 "surveyTitle". The component property tells redux-form that this
                 Field should appear as an HTML input tag. The type property
-                means the input need to be text. */}
-                    <Field type="text" name="surveyTitle" component="input" />
+                means the input need to be text.
+                <Field type="text" name="surveyTitle" component="input" /> */}
+                    {this.renderFields()}
                     <button type="submit">Submit</button>
                 </form>
             </div>
