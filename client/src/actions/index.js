29c0b4dd7d371a58176e0042eb6d08d4d92864c1
ywCN <wangyouhao90@gmail.com>
2018-01-05 18:45:19 -0500

    debugged: missing /

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -30,6 +30,6 @@ export const handleToken = token => async dispatch => {
 export const submitSurvey = (values, history) => async dispatch => {
     const res = await axios.post('/api/surveys', values);
 
-    history.push('surveys');
+    history.push('/surveys');
     dispatch({ type: FETCH_USER, payload: res.data });
 };
