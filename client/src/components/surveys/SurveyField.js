fa82fcd199ef8ddf57471f0a35de4722de8b85ed
ywCN <wangyouhao90@gmail.com>
2018-01-04 19:18:38 -0500

    init SurveyField

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+// SurveyField contains logic to render a single label and text input
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <input />
+        </div>
+    );
+};
