9c8aa99b4001a0fbfb9bcf64cc11338285cdc2d4
ywCN <wangyouhao90@gmail.com>
2018-01-05 18:01:13 -0500

    finished review form

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,12 +1,22 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
 
 const SurveyFormReview = ({ onCancel, formValues }) => {
+    const reviewFields = _.map(formFields, ({ label, name }) => {
+        return (
+            <div key={name}>
+                <label>{label}</label>
+                <div>{formValues[name]}</div>
+            </div>
+        );
+    });
+
     return (
         <div>
             <h5>Please confirm.</h5>
-
+            {reviewFields}
             <button className="yellow darken-3 btn-flat" onClick={onCancel}>
                 Back
             </button>
