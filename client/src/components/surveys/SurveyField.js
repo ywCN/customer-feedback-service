b3692764f5b08ee82dcc0e89ce338dd2a2fbdce7
ywCN <wangyouhao90@gmail.com>
2018-01-05 15:42:21 -0500

    log error info

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,7 +2,8 @@
 import React from 'react';
 
 // destructure, input is one property that contains a lot of event handlers
-export default ({ input, label }) => {
+export default ({ input, label, meta }) => {
+    console.log(meta); // meta contains error property
     return (
         <div>
             <label>{label}</label>
