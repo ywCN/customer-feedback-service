7a9c22f7d11a741e24cbe60c8a88acf928152bce
ywCN <wangyouhao90@gmail.com>
2018-01-05 18:14:37 -0500

    use injected submitSurvey

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,8 +2,9 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
     const reviewFields = _.map(formFields, ({ label, name }) => {
         return (
             <div key={name}>
@@ -20,7 +21,10 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
             <button className="yellow darken-3 btn-flat" onClick={onCancel}>
                 Back
             </button>
-            <button className="green btn-flat right white-text">
+            <button
+                onClick={() => submitSurvey(formValues)}
+                className="green btn-flat right white-text"
+            >
                 Send Survey
                 <i className="material-icons right">email</i>
             </button>
@@ -35,4 +39,4 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(SurveyFormReview);
